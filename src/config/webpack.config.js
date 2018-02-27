9474a38764f092bff906974a598c7aa1e81d6d2d
Gert Sønderby <gert.sonderby@orckestra.com>
2018-02-27 15:17:53 +0100

    Dev server with hot reload, will hot-replace React components.

--- src/config/webpack.config.js
+++ src/config/webpack.config.js
@@ -1,11 +1,15 @@
+const webpack = require('webpack');
 const path = require('path');
+const { parseEnv } = require('../utils');
+
+const ASSET_PATH = process.env.ASSET_PATH || '/';
 
 module.exports = {
 	mode: 'development',
 	entry: path.resolve('./src/index.js'),
 	output: {
 		filename: 'bundle.js',
-		path: path.resolve(process.cwd(), 'dist')
+		path: path.resolve(process.cwd(), 'dist'),
 	},
 	module: {
 	  rules: [
@@ -23,5 +27,23 @@ module.exports = {
 		modules: [
 			path.resolve('./node_modules')
 		],
-	}
+	},
+	plugins: [
+    // This makes it possible for us to safely use env vars on our code
+    new webpack.DefinePlugin({
+      'process.env.ASSET_PATH': JSON.stringify(ASSET_PATH)
+    })
+  ]
 };
+
+if (parseEnv('NODE_ENV') === 'development') {
+	module.exports.devtool = 'inline-source-map';
+	module.exports.devServer = {
+		contentBase: './dist',
+		hotOnly: true
+	};
+	module.exports.plugins.push(
+		new webpack.NamedModulesPlugin(),
+		new webpack.HotModuleReplacementPlugin()
+	);
+}
