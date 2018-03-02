94adb95b100b9136666c480b783a783c36cba1dc
Gert Sønderby <gert.sonderby@orckestra.com>
2018-03-02 17:12:48 +0100

    Added Immutable.js support, custom assertion to test basic reducer functionality.

--- src/config/unexpected.js
+++ src/config/unexpected.js
@@ -1,8 +1,30 @@
 const unexpected = require("unexpected");
 const unexpectedReact = require("unexpected-react");
 const unexpectedSinon = require("unexpected-sinon");
+const unexpectedImmutable = require("unexpected-immutable");
+const Immutable = require("immutable");
 
 global.expect = unexpected
 	.clone()
 	.use(unexpectedReact)
-	.use(unexpectedSinon);
+	.use(unexpectedSinon)
+	.use(unexpectedImmutable)
+	.addAssertion(
+		"<function> to be a reducer with initial state <object>",
+		function(expect, subject, initialState) {
+			expect.errorMode = "nested";
+			const oldState = Immutable.Map();
+			return expect(
+				subject,
+				"when called with",
+				[oldState, { type: "NOT_A_USEFUL_ACTION" }],
+				"to be",
+				oldState,
+			).and(
+				"when called with",
+				[undefined, { type: "@@INIT" }],
+				"to equal",
+				Immutable.fromJS(initialState),
+			);
+		},
+	);
