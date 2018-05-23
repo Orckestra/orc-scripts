ba54621723b0d0f5e1d8b693de4ba196ada3d733
Gert Sønderby <gert.sonderby@orckestra.com>
2018-05-23 10:50:58 +0200

    Removed some Flow adaptations, added some override capability to CLI builds

--- src/scripts/build/cli.js
+++ src/scripts/build/cli.js
@@ -1,30 +1,29 @@
 const path = require("path");
-const { resolveBin } = require("../../utils");
-
+const { hasPkgProp, resolveBin, hasFile } = require("../../utils");
 const spawn = require("cross-spawn");
+
+const here = p => path.join(__dirname, p);
 
 const args = process.argv.slice(2);
 
-const outDir = ["--out-dir", "dist"];
+const useSpecifiedOutDir = args.includes("--out-dir");
+const outDir = useSpecifiedOutDir ? [] : ["--out-dir", "dist"];
 
 const copyFiles = args.includes("--no-copy-files") ? [] : ["--copy-files"];
 
-const ignore = ["--ignore", ".test.js"];
+const useBuiltinConfig =
+	!args.includes("--presets") && !hasFile(".babelrc") && !hasPkgProp("babel");
+const config = useBuiltinConfig
+	? ["--presets", here("../../config/babelrc.js")]
+	: [];
 
-const babelConfig = require("../../config/babelrc.js");
-const fs = require("fs");
-const tempConfPath = path.resolve(process.cwd(), "babel.tmp.json");
-fs.writeFileSync(tempConfPath, JSON.stringify(babelConfig));
-
-const config = ["--presets", tempConfPath];
+const ignore = args.includes("--ignore") ? [] : ["--ignore", ".test.js"];
 
 const result = spawn.sync(
 	resolveBin("babel-cli", { executable: "babel" }),
 	[...outDir, ...copyFiles, ...ignore, ...config, "src"].concat(args),
 	{ stdio: "inherit" },
 );
-
-fs.unlinkSync(tempConfPath);
 
 if (result.status !== 0) {
 	console.error("Babel finished with non-zero exit code", result.status);
