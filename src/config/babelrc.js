cab3ec9eb7670c74bb068645eb39752c697633cc
Gert Sønderby <gert.sonderby@orckestra.com>
2018-10-29 13:53:15 +0100

    Removed babel preset react-app, upgraded to Babel 7.

--- src/config/babelrc.js
+++ src/config/babelrc.js
@@ -1,31 +1,33 @@
 const { parseEnv } = require("../utils");
 
-const isTest = (process.env.BABEL_ENV || process.env.NODE_ENV) === "test";
 const isWebpack = parseEnv("BUILD_WEBPACK", false);
 const isReact = parseEnv("BUILD_REACT", isWebpack);
 
-const envTargets = isTest
-	? { node: "current" }
-	: isWebpack
-		? ["defaults", "IE 11"]
-		: { node: "4.5" };
-
-const envOptions = { loose: true, targets: envTargets };
-
-module.exports = {
-	presets: [[require.resolve("babel-preset-env"), envOptions]],
-	plugins: [
+module.exports = function(api) {
+	const isTest = api.env() === "test";
+	const envTargets = isTest
+		? { node: "current" }
+		: isWebpack
+			? ["defaults", "IE 11"]
+			: { node: "4.5" };
+	const envOptions = { loose: true, targets: envTargets };
+	const presets = [[require.resolve("@babel/preset-env"), envOptions]];
+	const plugins = [
 		isReact ? require.resolve("babel-plugin-styled-components") : null,
-		require.resolve("babel-plugin-transform-es2015-template-literals"),
+		require.resolve("@babel/plugin-transform-template-literals"),
 		isReact ? require.resolve("react-hot-loader/babel") : null,
-		isReact ? require.resolve("babel-plugin-transform-react-jsx") : null,
+		isReact ? require.resolve("@babel/plugin-transform-react-jsx") : null,
 		isReact
 			? [
 					require.resolve("babel-plugin-react-intl-auto"),
 					{ removePrefix: "src", filebase: true },
 			  ]
 			: null,
-		require.resolve("babel-plugin-transform-object-rest-spread"),
-		require.resolve("babel-plugin-transform-es2015-computed-properties"),
-	].filter(x => !!x),
+		require.resolve("@babel/plugin-proposal-object-rest-spread"),
+		require.resolve("@babel/plugin-transform-computed-properties"),
+	].filter(x => !!x);
+	return {
+		presets,
+		plugins,
+	};
 };
