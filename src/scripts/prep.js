bcc07ce66344f2864fa6f956aa94caf305600292
Gert Sønderby <gert.sonderby@orckestra.com>
2019-08-15 15:08:22 +0200

    prep script now works if either or both of src/content and src/static is missing

--- src/scripts/prep.js
+++ src/scripts/prep.js
@@ -1,35 +1,25 @@
 const util = require("util");
-const fs = require("fs");
 const path = require("path");
 const makeDir = require("make-dir");
 
-const readdir = util.promisify(fs.readdir);
-const copyFile = util.promisify(fs.copyFile);
+const readdir = util.promisify(require("fs").readdir);
+const copyFile = util.promisify(require("ncp").ncp);
 
 const distDir = path.resolve(process.cwd(), "dist");
 const contentDir = path.resolve(process.cwd(), "src/content");
 const staticDir = path.resolve(process.cwd(), "src/static");
 
-makeDir("dist").then(() =>
-	readdir(contentDir)
-		.then(files =>
-			makeDir("dist/content").then(
-				Promise.all(
-					files.map(file =>
-						copyFile(
-							path.resolve(contentDir, file),
-							path.resolve(distDir, "content", file),
-						),
-					),
-				),
-			),
-		)
-		.then(() => {
-			// If production, don't copy static files
-			if (process.env.NODE_ENV === "production") process.exit(0);
-		})
-		.then(() =>
-			readdir(staticDir).then(files =>
+makeDir("dist")
+	.then(() =>
+		copyFile(contentDir, path.resolve(distDir, "content")).catch(() => {}),
+	)
+	.then(() => {
+		// If production, don't copy static files
+		if (process.env.NODE_ENV === "production") process.exit(0);
+	})
+	.then(() =>
+		readdir(staticDir)
+			.then(files =>
 				Promise.all(
 					files.map(file =>
 						copyFile(
@@ -38,6 +28,6 @@ makeDir("dist").then(() =>
 						),
 					),
 				),
-			),
-		),
-);
+			)
+			.catch(() => {}),
+	);
