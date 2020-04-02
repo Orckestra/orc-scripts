f4d97cd143694373b1cba6a1ce46724bf8ee3bf5
Gert Sønderby <gert.sonderby@orckestra.com>
2020-04-02 12:05:22 +0200

    Made buildDep script better able to handle branches

--- src/scripts/buildDep.js
+++ src/scripts/buildDep.js
@@ -13,17 +13,21 @@ if (!repos) {
 	process.exit(-1);
 }
 
+const develop = process.argv.includes("--develop");
 let release;
 if (process.argv.includes("--release")) {
 	release = process.argv[process.argv.indexOf("--release") + 1];
 }
+
+const releaseBranch = develop ? "develop" : release ? "release/" + release : "master";
 
 async function build(repos) {
 	const name = repos.match(/([^/]*)\.git$/)[1];
 	const folder = path.resolve(process.cwd(), "builds/" + name);
 	let pack;
 
-	console.log(`Building ${name + (release ? " " + release : "")} from repository ${repos}
+	const versionString = name + " " + releaseBranch;
+	console.log(`Building ${versionString} from repository ${repos}
 		in ${folder}`);
 
 	const buildResult = spawn.sync("npm", ["run", "build"]);
@@ -39,63 +43,64 @@ async function build(repos) {
 	const packName = packResult.stdout.toString("utf-8").trim();
 	console.log("Packaged to " + packName);
 
-	try {
-		await rimraf(folder);
-	} catch (err) {
-		console.error(err.message);
-		return -3;
-	}
-
-	try {
-		await makeDir("builds");
-	} catch (_) {}
-
-	const releaseArgs = release
-		? ["--branch", "release/" + release, "--single-branch"]
-		: ["--single-branch"];
-
-	const gitResult = spawn.sync("git", ["clone", repos, ...releaseArgs, folder]);
-	if (gitResult.status !== 0) {
-		console.error(gitResult.stderr.toString("utf-8"));
-		return -4;
-	}
-	console.log("Git repository was cloned");
-
-	try {
-		await copyFile(path.resolve(folder, ".env.example"), path.resolve(folder, ".env"));
-	} catch (err) {
-		console.error(err.message);
-		return -5;
-	}
-
-	const installResult = spawn.sync("npm", ["install"], { cwd: folder });
-	if (installResult.status !== 0) {
-		console.error(installResult.stderr.toString("utf-8"));
-		return -6;
-	}
-	console.log("Dependencies installed");
-
 	pack = path.resolve(process.cwd(), packName);
-	const packInstallResult = spawn.sync("npm", ["install", pack], {
-		cwd: folder,
-	});
-	if (packInstallResult.status !== 0) {
-		console.error(packInstallResult.stderr.toString("utf-8"));
-		return -7;
-	}
-	console.log("Package installed");
+	try {
+		try {
+			await rimraf(folder);
+		} catch (err) {
+			console.error(err.message);
+			return -3;
+		}
 
-	const testResult = spawn.sync("npm", ["test", "--", "--no-watch"], {
-		cwd: folder,
-		stdio: "inherit",
-	});
-	if (testResult.status !== 0) {
-		return 1;
-	}
+		try {
+			await makeDir("builds");
+		} catch (_) {}
 
-	console.log("Tests passed");
-	if (pack) await rimraf(pack);
-	return 0;
+		const releaseArgs = ["--branch", releaseBranch, "--single-branch"];
+
+		const gitResult = spawn.sync("git", ["clone", repos, ...releaseArgs, folder]);
+		if (gitResult.status !== 0) {
+			console.error(gitResult.stderr.toString("utf-8"));
+			return -4;
+		}
+		console.log("Git repository was cloned");
+
+		try {
+			await copyFile(path.resolve(folder, ".env.example"), path.resolve(folder, ".env"));
+		} catch (err) {
+			console.error(err.message);
+			return -5;
+		}
+
+		const installResult = spawn.sync("npm", ["install"], { cwd: folder });
+		if (installResult.status !== 0) {
+			console.error(installResult.stderr.toString("utf-8"));
+			return -6;
+		}
+		console.log("Dependencies installed");
+
+		const packInstallResult = spawn.sync("npm", ["install", pack], {
+			cwd: folder,
+		});
+		if (packInstallResult.status !== 0) {
+			console.error(packInstallResult.stderr.toString("utf-8"));
+			return -7;
+		}
+		console.log("Package installed");
+
+		const testResult = spawn.sync("npm", ["test", "--", "--no-watch"], {
+			cwd: folder,
+			stdio: "inherit",
+		});
+		if (testResult.status !== 0) {
+			return 1;
+		}
+
+		console.log("Tests passed");
+		return 0;
+	} finally {
+		if (pack) await rimraf(pack);
+	}
 }
 
 build(repos).then(returnCode => {
