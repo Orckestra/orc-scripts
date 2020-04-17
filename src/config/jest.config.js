c605e6f9ef96dd7547406fbc8ae0000eb60654db
Gert Sønderby <gert.sonderby@orckestra.com>
2020-04-17 15:41:29 +0200

    Remove cutout for transform rules in jest config

--- src/config/jest.config.js
+++ src/config/jest.config.js
@@ -1,13 +1,10 @@
 const path = require("path");
-const { hasFile, hasPkgProp, fromRoot } = require("../utils");
+const { fromRoot } = require("../utils");
 
 const here = p => path.join(__dirname, p);
 
 const args = process.argv.slice(2);
 const isCI = require("is-ci") || args.includes("--ci");
-
-const useBuiltInBabelConfig =
-	(!hasFile(".babelrc") || !hasFile("babel.config.js")) && !hasPkgProp("babel");
 
 const ignores = ["/node_modules/"];
 
@@ -25,10 +22,14 @@ const jestConfig = {
 	globals: {
 		BUILD_ID: "000",
 		BUILD_NUMBER: "000",
-		OVERTURE_MODULE: "aModule"
+		OVERTURE_MODULE: "aModule",
 	},
 	setupFiles: [require.resolve("whatwg-fetch")],
 	setupFilesAfterEnv: [here("unexpected.js")],
+	transform: { "^.+\\.js$": here("./babel-transform") },
+	transformIgnorePatterns: [
+		"/node_modules/(?!(?:" + require("./babel-whitelist.json").join("|") + ")/)",
+	],
 	testMatch: ["**/*.test.js"],
 	testPathIgnorePatterns: [...ignores],
 	testURL: "http://localhost:8000/",
@@ -37,14 +38,6 @@ const jestConfig = {
 
 if (!isCI) {
 	jestConfig["coverageReporters"] = ["html", "text-summary"];
-}
-
-if (useBuiltInBabelConfig) {
-	jestConfig["transform"] = { "^.+\\.js$": here("./babel-transform") };
-	const whitelist = require("./babel-whitelist.json");
-	jestConfig["transformIgnorePatterns"] = [
-		"/node_modules/(?!(?:" + whitelist.join("|") + ")/)",
-	];
 }
 
 module.exports = jestConfig;
