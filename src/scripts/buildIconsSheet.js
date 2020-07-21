b30bafd8dbb4293b709634b15eef1f986b9b4109
ruslan.avsenin <ruslan.avsenin@orckestra.com>
2020-07-21 13:52:03 +0300

    build icons sheet script was added

---
+++ src/scripts/buildIconsSheet.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const baseDir = path.resolve(process.cwd(), "src");
+const content = path.resolve(baseDir, "content");
+const icons = path.resolve(content, "icons");
+const fs = require("fs");
+
+const iconsSheet = path.resolve(content, "iconsSheet.svg");
+
+const outStream = fs.createWriteStream(iconsSheet);
+outStream.addListener("open", () => {
+	outStream.write('<svg xmlns="http://www.w3.org/2000/svg">\n');
+	// Beware of any files with spaces in filenames, they're bad
+	const fileList = fs.readdirSync(icons).filter(file => file.endsWith(".svg"));
+
+	fileList.forEach(fileName => {
+		const pathToIcon = path.resolve(icons, fileName);
+
+		const fileNameWithoutExtension = fileName.replace(".svg", "");
+
+		const svgContent = fs.readFileSync(pathToIcon).toString();
+
+		const strippedContent = svgContent
+			.replace(/<?(.*)?>/, "")
+			.replace(/<!--(.*)-->/, "")
+			.replace(/<title>(.*)<\/title>/, "")
+			.replace(/<desc>(.*)<\/desc>/, "")
+			.replace("svg", "symbol")
+			.replace("</svg>", "</symbol>\n")
+			.replace("<symbol", `<symbol id="${fileNameWithoutExtension}"`)
+			.replace(/<g id="(.*?)"/, "<g")
+			.replace(/stroke="(.*?)" /, "")
+			.replace(/stroke-width="(.*?)" /, "")
+			.replace(/fill="(.*?)" /, "")
+			.replace(/width(.*)px" /, "")
+			.replace(/ version(.*)>/, ">")
+			.replace(/ fill="(.*?)"/, "")
+			.replace(/[!@$^&%*+[\]{}|?\\]/g, "")
+			.replace(/^\s*[\r\n]/gm, "");
+
+		outStream.write(strippedContent);
+	});
+	outStream.end("</svg>");
+});
